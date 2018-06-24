be3d38a1b6bbf55bc179f6562c8a47d842dd7b5f
ky-yk-d <key0of7one0s5life@gmail.com>
2018-06-24 10:32:35 +0900

    getMessageをモジュールに移植

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // ベースはQiita等で様々な人が公開しているソースコード
 // （例） https://qiita.com/tenn25/items/5456f9eb6ac92ff09dd9
 let https = require('https');
+const Message = require('./src/message');
 
 /**
  * Lambda実行時に呼び出されるハンドラ
@@ -23,7 +24,8 @@ exports.handler = (event, context, callback) => {
 // https://developers.line.me/ja/docs/messaging-api/reference/#anchor-e65d8a1fb213489f6475b06ad10f66b7b30b0072
     jsonFile = require("./dialogue.json");
 // 入力に応じたメッサージの選択
-    messageObj = getMessageObj(data, jsonFile);
+    messageObj = Message.getMessageObj(data,jsonFile);
+    // messageObj = getMessageObj(data, jsonFile);
 // 返すデータを作成する
     console.log('データ作成');
     replyData = JSON.stringify({
@@ -56,43 +58,4 @@ exports.handler = (event, context, callback) => {
     callback(null, replyData);
     req.write(replyData);
     req.end();
-};
-
-/**
- * 入力されたデータに応じて、返すメッセージを生成する関数
- * @param {} data 入力されたデータ
- * @param {} jsonFile 外部ファイルから読み込んだJSON形式のデータ
- * @return {} メッセージオブジェクト
- * @author こまど（ky_yk_d）
- */
-let getMessageObj = (data, jsonFile)=> {
-    switch (data.type){
-        case 'message':
-            console.log('メッセージの場合');
-            if (data.message.type == 'text'){
-                // テキストメッセージの場合、入力された文字列に応じて分岐
-                if (data.message.text == '住所') {
-                    return jsonFile.dialogue2;
-                } else {
-                    return jsonFile.dialogue1;
-                }
-            } else if (data.message.type == "image"){
-                console.log('画像メッセージ');
-                return jsonFile.imageMessage;
-            } else if (data.message.type == "sticker"){
-                console.log('スタンプメッセージ');
-                return jsonFile.stickerMessage;
-            } else {
-                // テキストメッセージ以外の場合
-                console.log('テキスト以外のメッセージが入力された');
-                return jsonFile.otherMessageType;
-            }
-        case 'postback':
-            console.log('postbackの場合');
-            return jsonFile[data.postback.data];
-        default :
-            console.log('それ以外の場合');
-            console.log(data);
-            return jsonFile.otherEventType;
-    }
 };
